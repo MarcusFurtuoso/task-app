5fcd800ddc78769ce0df8f40dba4e97a7cf03820
MarcusFurtuoso <marcusvini678@gmail.com>
2024-02-20 02:30:02 -0300

    config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBT5JZW84aa1cTFs8wfzrb8do0qh6UFVFA",
+    authDomain: "taskapp-52224.firebaseapp.com",
+    projectId: "taskapp-52224",
+    storageBucket: "taskapp-52224.appspot.com",
+    messagingSenderId: "42789827998",
+    appId: "1:42789827998:web:6ae78216081b3ca767528f"
+  }
 };
 
 /*
