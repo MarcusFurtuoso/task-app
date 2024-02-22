e2680ee29e2292e266c9dfa14f037d00f1f9b176
MarcusFurtuoso <marcusvini678@gmail.com>
2024-02-21 21:31:19 -0300

    add shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,18 +6,32 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CustomInputComponent } from './components/custom-input/custom-input.component';
 import { LogoComponent } from './components/logo/logo.component';
-
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { AddUpdateTaskComponent } from './components/add-update-task/add-update-task.component';
 
 @NgModule({
-  declarations: [HeaderComponent, CustomInputComponent, LogoComponent],
-  exports: [HeaderComponent, CustomInputComponent, LogoComponent],
+  declarations: [HeaderComponent, CustomInputComponent, LogoComponent, AddUpdateTaskComponent],
+  exports: [
+    HeaderComponent,
+    CustomInputComponent,
+    LogoComponent,
+    NgCircleProgressModule,
+    AddUpdateTaskComponent,
+  ],
   imports: [
     CommonModule,
     IonicModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-  ]
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+    }),
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
